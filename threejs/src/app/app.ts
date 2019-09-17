d84ecd5516bf0bdfef2111497848c0fc041f6ee3
Daniel Munday <mundaymeister@gmail.com>
2019-09-17 14:21:10 +0100

    threejs start

---
+++ threejs/src/app/app.ts
@@ -0,0 +1,72 @@
+import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
+import { Brick } from './brick';
+import { WEBVR } from 'three/examples/jsm/vr/WebVR.js';
+
+export class App {
+  private readonly scene = new Scene();
+  private readonly camera = new PerspectiveCamera(70, innerWidth / innerHeight, 0.1, 10000);
+  private readonly renderer = new WebGLRenderer({
+    antialias: true,
+    canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
+  });
+
+  private brick: Brick;
+  private bricks: Brick[];
+
+  constructor() {
+    this.brick = new Brick(5, new Color('rgb(0,255,0)'));
+    this.scene.add(this.brick);
+    this.brick.position.set(10,0,10);
+    this.bricks = [];
+
+    for(let x = 0; x < 10; x++) {
+      for(let y = 0; y < 10; y++) {
+        for(let z = 0; z < 10; z++) {
+          const bricky = new Brick(1, new Color(`rgb(${x*25},${y*25},${z*25})`));
+          console.log('added brick:', (x*10) -50,(y*10) -50,(z*10) -50)
+          bricky.position.set((x*10) -50,(y*10) -50,(z*10) -50);
+          this.bricks.push(bricky);
+        }
+      }
+    }
+    this.bricks.forEach(brickk => {this.scene.add(brickk);})
+    // this.scene.add(this.bricks);
+
+    this.camera.position.set(0,0,0);
+    this.camera.lookAt(new Vector3(1, 1, 1));
+
+    this.renderer.setSize(innerWidth, innerHeight);
+    this.renderer.setClearColor(new Color('rgb(0,0,0)'));
+
+    document.body.appendChild( WEBVR.createButton( this.renderer, {referenceSpaceType: 'false'} ) );
+    this.renderer.vr.enabled = true;
+
+    // this.render();
+    this.renderer.setAnimationLoop(this.erkkiTestRender);
+    this.camera.lookAt(new Vector3(1, 1, 1));
+  }
+
+  private adjustCanvasSize() {
+    this.renderer.setSize(innerWidth, innerHeight);
+    this.camera.aspect = innerWidth / innerHeight;
+    this.camera.updateProjectionMatrix();
+  }
+
+  // private render() {
+  //   this.renderer.render(this.scene, this.camera);
+  //   requestAnimationFrame(() => this.render());
+
+  //   this.adjustCanvasSize();
+  //   this.brick.rotateY(0.03);
+  // }
+
+  private erkkiTestRender = () => {
+    console.log('render?', this.camera.position.x, this.camera.position.y, this.camera.position.z, this.camera.rotation.x, this.camera.rotation.y, this.camera.rotation.z);
+    this.renderer.render(this.scene, this.camera);
+    this.adjustCanvasSize();
+    // this.camera.position.set(0,0,0);
+    // this.camera.lookAt(new Vector3(1, 1, 1));
+    this.brick.rotateY(0.03);
+    this.bricks.forEach(brickk => {brickk.rotateY(0.03)})
+  }
+}
