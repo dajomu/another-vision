50c93b5637c623b7a4dcde0e446c1cab63da3f3e
Erkks <erkki.antonkov@gmail.com>
2019-10-31 11:45:45 +0000

    Changed room from a box to a sphere to remove corners

--- threejs/src/app/room.ts
+++ threejs/src/app/room.ts
@@ -1,9 +1,9 @@
-import { BoxGeometry, DoubleSide, Color, Mesh, MeshBasicMaterial } from 'three';
+import { SphereGeometry, DoubleSide, Color, Mesh, MeshBasicMaterial } from 'three';
 
 export class Room extends Mesh {
   constructor(size: number, color: Color) {
     super();
-    this.geometry = new BoxGeometry(size, size, size);
+    this.geometry = new SphereGeometry(size, size, size);
     this.material = new MeshBasicMaterial({ color, side: DoubleSide });
   }
 }
