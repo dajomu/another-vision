438e1bf001490695c913ea043d81fce1bf5ad337
Erkks <erkki.antonkov@gmail.com>
2019-10-31 12:13:33 +0000

    Tidy up

--- threejs/src/app/app.ts
+++ threejs/src/app/app.ts
@@ -5,14 +5,11 @@ import { WEBVR } from 'three/examples/jsm/vr/WebVR.js';
 
 export class App {
   private readonly scene = new Scene();
-  private readonly camera = new PerspectiveCamera(70, innerWidth / innerHeight, 0.1, 10000);
+  private readonly camera = new PerspectiveCamera(90, innerWidth / innerHeight, 0.1, 10000);
   private readonly renderer = new WebGLRenderer({
     antialias: true,
     canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
   });
-
-  // private brick: Brick;
-  // private bricks: Brick[];
 
   private screen: Screen;
   private room: Room;
@@ -20,49 +17,32 @@ export class App {
   constructor() {
     // IMPORTANT - vr viewer starts off looking in (0, 0, -1) direction
 
+    // Screen
     this.screen = new Screen(10, 5, new Color('white'));
     this.scene.add(this.screen);
     this.screen.position.set(0,0,-10);
 
+    // Room
     this.room = new Room(20, new Color('red'));
     this.scene.add(this.room);
-    this.room.position.set(0,0,-10);
+    this.room.position.set(0,0,0);
 
-
-    // this.brick = new Brick(5, new Color('rgb(255,0,0)'));
-    // this.scene.add(this.brick);
-    // this.brick.position.set(0,0,-10);
-    // this.bricks = [];
-
-    // for(let x = 0; x < 10; x++) {
-    //   for(let y = 0; y < 10; y++) {
-    //     for(let z = 0; z < 10; z++) {
-    //       const bricky = new Brick(1, new Color(`rgb(${x*25},${y*25},${z*25})`));
-    //       console.log('added brick:', (x*10) -50,(y*10) -50,(z*10) -50)
-    //       bricky.position.set((x*10) -50,(y*10) -50,(z*10) -50);
-    //       this.bricks.push(bricky);
-    //     }
-    //   }
-    // }
-    // this.bricks.forEach(brickk => {this.scene.add(brickk);})
-    // this.scene.add(this.bricks);
-
+    // To see XYZ axes in VR
     var axesHelper = new AxesHelper( 20 );
     axesHelper.position.set(0, 0, 0);
     this.scene.add( axesHelper );
 
+    // Camera initialisation
     this.camera.position.set(0,0,0);
     this.camera.lookAt(new Vector3(1, 1, 1));
-
-    this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.setClearColor(new Color('rgb(0,0,0)'));
-
+    
+    // Add VR button
     document.body.appendChild( WEBVR.createButton( this.renderer, {referenceSpaceType: 'false'} ) );
     this.renderer.vr.enabled = true;
 
-    // this.render();
-    this.renderer.setAnimationLoop(this.erkkiTestRender);
-    // this.camera.lookAt(new Vector3(1, 1, 1));
+    this.renderer.setAnimationLoop(this.render);
+    this.renderer.setSize(innerWidth, innerHeight);
+    this.renderer.setClearColor(new Color('rgb(0,0,0)'));
   }
 
   private adjustCanvasSize() {
@@ -71,22 +51,15 @@ export class App {
     this.camera.updateProjectionMatrix();
   }
 
-  // private render() {
-  //   this.renderer.render(this.scene, this.camera);
-  //   requestAnimationFrame(() => this.render());
-
-  //   this.adjustCanvasSize();
-  //   this.brick.rotateY(0.03);
-  // }
-
-  private erkkiTestRender = () => {
-    console.log('render?', this.camera.position.x, this.camera.position.y, this.camera.position.z, this.camera.rotation.x, this.camera.rotation.y, this.camera.rotation.z);
+  private render = () => {
+    console.log('render:', 
+      'X-pos: ' + this.camera.position.x,
+      'Y-pos: ' + this.camera.position.y, 
+      'Z-pos: ' + this.camera.position.z, 
+      'X-rot: ' + this.camera.rotation.x, 
+      'Y-rot: ' + this.camera.rotation.y, 
+      'Z-rot: ' + this.camera.rotation.z);
     this.renderer.render(this.scene, this.camera);
     this.adjustCanvasSize();
-    // this.screen.rotateY(0.03);
-    // this.camera.position.set(0,0,0);
-    // this.camera.lookAt(new Vector3(1, 1, 1));
-    // this.brick.rotateY(0.03);
-    // this.bricks.forEach(brickk => {brickk.rotateY(0.03)})
   }
 }
