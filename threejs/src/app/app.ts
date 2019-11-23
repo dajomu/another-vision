1b5c7ebce0b384f21e64f805414a3d565ef1182f
Daniel Munday <mundaymeister@gmail.com>
2019-11-23 16:16:57 +0000

    adding room obj (incomplete)

--- threejs/src/app/app.ts
+++ threejs/src/app/app.ts
@@ -1,8 +1,10 @@
-import { AxesHelper, Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
+import { AxesHelper, Color, PerspectiveCamera, Scene, SpotLight, Vector3, WebGLRenderer } from 'three';
 import { Room } from './room';
 import { Screen } from './screen';
 import { WEBVR } from 'three/examples/jsm/vr/WebVR.js';
-import THREE = require('three');
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
+
+const loader = new OBJLoader();
 
 export class App {
   private readonly scene = new Scene();
@@ -31,7 +33,7 @@ export class App {
     // Light
     const color = 0x35fc03;
     const intensity = 0.75;
-    const light = new THREE.SpotLight(color, intensity);
+    const light = new SpotLight(color, intensity);
     light.position.set(0, 0, 100);
     light.target.position.set(0, 0, -1);
 
@@ -46,7 +48,9 @@ export class App {
     // Camera initialisation
     this.camera.position.set(0,0,0);
     this.camera.lookAt(new Vector3(1, 1, 1));
-    
+
+    this.loadRoom(this);
+
     // Add VR button
     document.body.appendChild( WEBVR.createButton( this.renderer, {referenceSpaceType: 'false'} ) );
     this.renderer.vr.enabled = true;
@@ -56,6 +60,31 @@ export class App {
     this.renderer.setClearColor(new Color('rgb(0,0,0)'));
   }
 
+  private loadRoom = (that: any) => {
+    loader.load(
+      // resource URL
+      './dist/assets/room1.obj',
+      // called when resource is loaded
+      function ( object: any ) {
+
+        that.scene.add( object );
+
+      },
+      // called when loading is in progresses
+      function ( xhr: any ) {
+
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+      },
+      // called when loading has errors
+      ( error: any ) => {
+
+        console.log( 'An error happened' );
+
+      }
+    );
+  }
+
   private adjustCanvasSize() {
     this.renderer.setSize(innerWidth, innerHeight);
     this.camera.aspect = innerWidth / innerHeight;
@@ -63,12 +92,12 @@ export class App {
   }
 
   private render = () => {
-    console.log('render:', 
+    console.log('render:',
       'X-pos: ' + this.camera.position.x,
-      'Y-pos: ' + this.camera.position.y, 
-      'Z-pos: ' + this.camera.position.z, 
-      'X-rot: ' + this.camera.rotation.x, 
-      'Y-rot: ' + this.camera.rotation.y, 
+      'Y-pos: ' + this.camera.position.y,
+      'Z-pos: ' + this.camera.position.z,
+      'X-rot: ' + this.camera.rotation.x,
+      'Y-rot: ' + this.camera.rotation.y,
       'Z-rot: ' + this.camera.rotation.z);
     this.renderer.render(this.scene, this.camera);
     this.adjustCanvasSize();
