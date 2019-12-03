5f445d3fdbbcb3ac0d045cadf35406e05d500c85
Erkks <erkki.antonkov@gmail.com>
2019-12-03 10:10:38 +0000

    commented out bits we aren't using

--- threejs/src/app/app.ts
+++ threejs/src/app/app.ts
@@ -1,6 +1,6 @@
 import { AxesHelper, Color, DoubleSide, Fog, Mesh, MeshPhongMaterial, PerspectiveCamera, Scene, SpotLight, Vector3, WebGLRenderer, FogExp2 } from 'three';
-import { Room } from './room';
-import { Screen } from './screen';
+// import { Room } from './room';
+// import { Screen } from './screen';
 import { WEBVR } from 'three/examples/jsm/vr/WebVR.js';
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
 
@@ -14,18 +14,19 @@ export class App {
     canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
   });
 
+  // Currently not using as using room model
   // private screen: Screen;
   // private room: Room;
 
   constructor() {
     // IMPORTANT - vr viewer starts off looking in (0, 0, -1) direction
 
-    // Screen
+    // Screen - currently not using as using room model
     // this.screen = new Screen(10, 5, new Color('white'));
     // this.scene.add(this.screen);
     // this.screen.position.set(0,0,-10);
 
-    // Room
+    // Room - currently not using as using room model
     // this.room = new Room(20, new Color('red'));
     // this.scene.add(this.room);
     // this.room.position.set(0,0,0);
@@ -64,6 +65,7 @@ export class App {
     this.renderer.setClearColor(new Color('rgb(0,0,0)'));
   }
 
+  // Use room model
   private loadRoom = (that: any) => {
     loader.load(
       // resource URL
@@ -71,26 +73,25 @@ export class App {
       // called when resource is loaded
 
       function( obj ){
+        // apply material to object so that light reflects off it
         obj.traverse( function( child ) {
             if ( child instanceof Mesh ) {
                 child.material = new MeshPhongMaterial({ color: new Color('red'), side: DoubleSide });
             }
         } );
+        // need to figure out a good co-ordinate system to place camera, lights and room object
         obj.position.set(-0.25, 0, -2);
         obj.rotation.set(0, -Math.PI / 2, 0);
         that.scene.add( obj );
       },
+
       // called when loading is in progresses
       function ( xhr: any ) {
-
         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
       },
       // called when loading has errors
       ( error: any ) => {
-
         console.log( 'An error happened' );
-
       }
     );
   }
